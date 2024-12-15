4d66bc8c821ccb912fa05896892e4d906cc55ad0
metrdy <richard.mcshinsky@metrdy.com>
2024-12-15 15:05:46 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '1'
+VERSION = '11'
 DESCRIPTION = "Dynamic multiselect filters for Streamlit sorted by frequency"
 
 with open("README.md", "r", encoding="utf-8") as fh:
@@ -10,7 +10,7 @@ with open("CHANGELOG.md", "r", encoding="utf-8") as fh:
     changelog = fh.read()
 
 setup(
-    name="streamlit_dynamic_filters",
+    name="streamlit_dynamic_filters_sorting",
     version=VERSION,
     author="Oleksandr Arsentiev",
     author_email="<arsentiev9393@gmail.com>",
